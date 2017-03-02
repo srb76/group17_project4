fbad647d4a19fbb98f652282f8a8db888bf15ab7
DSchroederOSU <schrodan@oregonstate.edu>
2017-03-01 18:49:28 -0800

    Coordinate tests complete

--- src/Test/CoordinateTest.java
+++ src/Test/CoordinateTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 public class CoordinateTest {
     @Test
     void testGettersForAcrossAndDown(){
@@ -16,5 +18,23 @@ public class CoordinateTest {
 
         assertEquals(across, 2);
         assertEquals(down, 7);
+    }
+
+    @Test
+    void testEquals(){
+        Coordinate test = new Coordinate(2, 7);
+        Coordinate test2 = new Coordinate(2, 7);
+        Coordinate test3 = new Coordinate(1,1);
+        assertEquals(test.equals(test2), true);
+        assertEquals(test.equals(test3), false);
+    }
+
+    @Test
+    void testSettersForAcrossAndDown(){
+        Coordinate test = new Coordinate(2, 7);
+        test.setAcross(3);
+        test.setDown(5);
+        assertEquals(test.getAcross(), 3);
+        assertEquals(test.getDown(), 5);
     }
 }
